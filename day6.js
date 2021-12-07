ca21860d0b4d69d6506d4662f38f4559c595a9b5
Joni <joni28492@gmail.com>
2021-12-07 09:19:42 +0100

    day 6 success

--- day6.js
+++ day6.js
@@ -25,9 +25,9 @@ Una vez que tengas el resultado...
 
 function sumPairs(numbers = [], result = Number) {
   for (let i = 0; i < numbers.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (numbers[j] + numbers[i] === result) {
-        return [numbers[j], numbers[i]];
+    for (let j = 0; j < numbers.length; j++) {
+      if (i !== j && numbers[j] + numbers[i] === result) {
+        return [numbers[i], numbers[j]];
       }
     }
   }
@@ -38,7 +38,5 @@ function sumPairs(numbers = [], result = Number) {
 console.log(sumPairs([3, 5, 7, 2], 10)); // [3, 7]
 console.log(sumPairs([-3, -2, 7, -5], 10)); // null
 console.log(sumPairs([2, 2, 3, 1], 4)); // [2, 2]
-//lectura erronea en este hace 7+1
 console.log(sumPairs([6, 7, 1, 2], 8)); // [6, 2]
-
 console.log(sumPairs([0, 2, 2, 3, -1, 1, 5], 6)); // [1, 5]
